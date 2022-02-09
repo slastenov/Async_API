59b3185113b09a654dd9dd81235986b4aa75a556
Iaroslav Slastenov <yslastenov@asdco.ru>
2022-02-10 01:57:58 +0300

    add models

---
+++ src/models/base.py
@@ -0,0 +1,13 @@
+import orjson
+from pydantic import BaseModel
+
+
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
+
+
+class BaseApiModel(BaseModel):
+    """Базовый класс для моделей API."""
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
--- src/models/film.py
+++ src/models/film.py
@@ -1,19 +1,8 @@
-import orjson
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
-from pydantic import BaseModel
+from .base import BaseApiModel
 
 
-def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
-    return orjson.dumps(v, default=default).decode()
-
-
-class Film(BaseModel):
+class Film(BaseApiModel):
     id: str
     title: str
     description: str
 
-    class Config:
-        # Заменяем стандартную работу с json на более быструю
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
