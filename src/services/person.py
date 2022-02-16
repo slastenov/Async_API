73978b4dfe3edea9ff01f070628f9e7962b5dacb
Iaroslav Slastenov <yslastenov@asdco.ru>
2022-02-16 16:16:29 +0300

    add person search

--- src/services/person.py
+++ src/services/person.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import List, Optional
+from typing import Optional
 
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch, exceptions
@@ -7,9 +7,10 @@ from fastapi import Depends
 
 from db.elastic import get_elastic
 from db.redis import get_redis, async_cache
+from models.page import Page
 from models.person import Person
 
-GENRE_CACHE_EXPIRE_IN_SECONDS = 60 * 60  # 1 час
+GENRE_PERSON_EXPIRE_IN_SECONDS = 60 * 60  # 1 час
 
 
 class PersonService:
@@ -17,15 +18,31 @@ class PersonService:
         self.redis = redis
         self.elastic = elastic
 
-    @async_cache(Person, page=False, ttl=GENRE_CACHE_EXPIRE_IN_SECONDS)
+    @async_cache(Person, page=False, ttl=GENRE_PERSON_EXPIRE_IN_SECONDS)
     async def get_by_id(self, person_id: str) -> Optional[Person]:
         person = await self._get_person_from_elastic(person_id)
         return person
 
-    async def get_list(self) -> Optional[List[Person]]:
-        resp = await self.elastic.search(index="person", size=500)
-        persons = [Person(**doc["_source"]) for doc in resp["hits"]["hits"]]
-        return persons
+    @async_cache(Person, page=True, ttl=GENRE_PERSON_EXPIRE_IN_SECONDS)
+    async def search(self, query: str, page_size: int, page_number: int) -> Page[Person]:
+        body = {
+            "multi_match": {
+                "query": query,
+                "fields": [
+                    "full_name^3",
+                ],
+                "operator": "and",
+                "fuzziness": "AUTO"
+            }
+        }
+        start_page = (page_number - 1) * page_size
+        doc = await self.elastic.search(index='person', query=body, size=page_size, from_=start_page)
+        return Page(
+            items=[Person(**person["_source"]) for person in doc["hits"]["hits"]],
+            page_number=page_number,
+            page_size=page_size,
+            total=doc["hits"]["total"]["value"],
+        )
 
     async def _get_person_from_elastic(self, person_id: str) -> Optional[Person]:
         try:
